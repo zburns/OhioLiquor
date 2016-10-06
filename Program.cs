b03c2f7581106f2e8d145a2638cba6d7b9c62006
Zachary Burns <developer@zackburns.com>
2016-10-06 11:42:38 -0400

    Update Program.cs
    Removed duplicate code and added send mail routine

--- Program.cs
+++ Program.cs
@@ -17,25 +17,14 @@ private static void Main(string[] args)
     }
 }
 
- 
 
- 
-  private static void Main(string[] args)
+  private static void SendMail()
 {
-    if (File.Exists(new AppSettingsReader().GetValue("oldfile", Type.GetType("System.String")).ToString()))
+    if (File.Exists(new AppSettingsReader().GetValue("newfile", Type.GetType("System.String")).ToString()))
     {
-        new WebClient().DownloadFile(new AppSettingsReader().GetValue("URL", Type.GetType("System.String")).ToString(), new AppSettingsReader().GetValue("newfile", Type.GetType("System.String")).ToString());
-        if (new FileInfo(new AppSettingsReader().GetValue("oldfile", Type.GetType("System.String")).ToString()).Length != new FileInfo(new AppSettingsReader().GetValue("newfile", Type.GetType("System.String")).ToString()).Length)
-        {
-            SendMail();
-            File.Delete(new AppSettingsReader().GetValue("oldfile", Type.GetType("System.String")).ToString());
-            File.Copy(new AppSettingsReader().GetValue("newfile", Type.GetType("System.String")).ToString(), new AppSettingsReader().GetValue("oldfile", Type.GetType("System.String")).ToString());
-        }
-    }
-    else
-    {
-        new WebClient().DownloadFile(new AppSettingsReader().GetValue("URL", Type.GetType("System.String")).ToString(), new AppSettingsReader().GetValue("oldfile", Type.GetType("System.String")).ToString());
-        SendMail();
+        MailMessage message = new MailMessage("from@example.com", "to@enduser.com", "New Ohio Liquor List", "Attached is the new list of liquor");
+        message.Attachments.Add(new Attachment(new AppSettingsReader().GetValue("newfile", Type.GetType("System.String")).ToString()));
+        new SmtpClient("localhost", 25).Send(message);
     }
 }
 
